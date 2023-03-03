4d39e6f22a5161f6d7a82bafb317fd8d967e5b21
hojung_kwon@brown.edu <hojung_kwon@brown.edu>
2023-03-03 14:45:20 -0500

    VisualizeBodyMass modified so that it doesnt need server

--- VisualizeBodyMass.cs
+++ VisualizeBodyMass.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Networking;
 using UnityEngine.UI;
 using TMPro;
 using System.IO;
@@ -10,79 +9,52 @@ using System.Text;
 using System.Linq;
 using System;
 using System.Globalization;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 
 public class VisualizeBodyMass : MonoBehaviour
 {
     int currentSceneID;
-    string[] sceneTypes = {"forest.csv", "savanna.csv", "wetland.csv"};
-    private readonly string url = "http://10.38.190.131:8000/download";
-    
-    public class PrimateData
-    {
-        public string habitatType;
-        public string visType;
-    }
+    string[] sceneTypes = {"forest_bodymass.json", "savanna_bodymass.json", "wetland_bodymass.json"};
+
+    List<string> speciesNames = new List<string>();
+    List<float> bodyMass = new List<float>();
 
     // Start is called before the first frame update
     public void Start()
     {
         currentSceneID = SceneManager.GetActiveScene().buildIndex - 1;
-        // Debug.Log(sceneTypes[currentSceneID]);
-        StartCoroutine(PostDataToFetch(sceneTypes[currentSceneID]));
+
+        DataLoader(sceneTypes[currentSceneID]);
     }
 
     // Update is called once per frame
     public void GetData()
     {
         currentSceneID = SceneManager.GetActiveScene().buildIndex - 1;
-        // Debug.Log(sceneTypes[currentSceneID]);
-        // StartCoroutine(PostDataToFetch(sceneTypes[currentSceneID]));
+        //DataLoader(sceneTypes[currentSceneID]);
     }
 
-    IEnumerator PostDataToFetch(string sceneType)
+
+    // Loads data that is relevant to the current visualization
+    public void DataLoader(string sceneType)
     {
-        PrimateData primateDataToSend = new PrimateData();
-        primateDataToSend.habitatType = sceneType;
-        primateDataToSend.visType = "bodymass";
-
-        string bodyJsonString = JsonUtility.ToJson(primateDataToSend);
-        var request = new UnityWebRequest(url, "PUT");
-        byte[] bodyRaw = Encoding.UTF8.GetBytes(bodyJsonString);
-        
-        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
-        request.downloadHandler = (DownloadHandler)new DownloadHandlerBuffer();
-        request.SetRequestHeader("Content-Type", "application/json");
-        yield return request.SendWebRequest();
-
-        if (request.isNetworkError)
-        {
-            Debug.Log("Network Error: " + request.error);
-        }
-        else if (request.isHttpError)
-        {
-            Debug.Log("Http Error: " + request.error);
-        }
-        else
-        {
-            // Extract the data sent by the server
-            string receivedData = request.downloadHandler.text;
-            string editedData = receivedData.Replace("[", "");
-            string editedData2 = editedData.Replace("]", "");
-            string[] dataAsList = editedData2.Split(",");
-
-            // Divide data entries in dataAsList into two parallel lists, each containing primate names and body mass
-            List<string> PrimateNames = new List<string>();
-            List<float> BodyMass = new List<float>();
-            for (int i = 0; i < dataAsList.Length-1; i+=2)
+        using (StreamReader r = new StreamReader("Assets/"+sceneType))  
+        {  
+            string json = r.ReadToEnd();  
+            string dataEdited = json.Replace("{", "");
+            string dataEdited2 = dataEdited.Replace("}","");
+            string[] data = dataEdited2.Split(',');
+            Debug.Log(data.Length);
+            
+            for (int i = 0; i < data.Length; i ++)
             {
-                PrimateNames.Add(dataAsList[i]);
-                BodyMass.Add(float.Parse(dataAsList[i+1], CultureInfo.InvariantCulture.NumberFormat));
+                string[] dataEntry = data[i].Split(":");
+                speciesNames.Add(dataEntry[0]);
+                bodyMass.Add(float.Parse(dataEntry[1], CultureInfo.InvariantCulture.NumberFormat));
             }
-            Debug.Log(PrimateNames.Count);
-            Debug.Log(BodyMass.Count);
-
-            // To do: Spawn circles according to the sent data
         }
     }
 }
+
