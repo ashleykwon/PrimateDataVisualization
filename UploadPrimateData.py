ff8a1f76aab9c46e3c17bc1dd70c8213a73fa5b1
hojung_kwon@brown.edu <hojung_kwon@brown.edu>
2023-03-03 13:57:02 -0500

    modified UploadPrimateData

--- UploadPrimateData.py
+++ UploadPrimateData.py
@@ -1,33 +1,14 @@
-import uvicorn
-from fastapi import FastAPI
-import os 
 import numpy as np
 from PIL import Image
-from pydantic import BaseModel
 import json
 import csv
 
-app = FastAPI()
-
-class Input(BaseModel):
-    habitatType: str
-    visType: str
-
-# Downloads the JSON file sent by a Unity, fetches appropriate data, and sends it back to the Unity project
-@app.put("/download")
-def dataDownloader(d:Input):
-    habitatType = d.habitatType
-    visType = d.visType
-    dataToSend = dataUploader(visType, habitatType)
-    return dataToSend
-
-
-# Fetches requested data and processes it for the Unity project to retrieve
+# Generates JSON files of primate data for a C# script to read
 def dataUploader(visType, habitatType):
-    dataToSend = []
+    dataToSend = {}
     
     # Open the chosen CSV file
-    dataDir = 'path'
+    dataDir = 'C:/Users/Ashley Kwon/Desktop/PrimateData/'
     fileToRead = dataDir+habitatType
     lineNum = 0
 
@@ -36,21 +17,22 @@ def dataUploader(visType, habitatType):
         csv_reader = csv.reader(csv_file)
         for line in csv_reader:
             if lineNum != 0: #don't include metadata
-                # dataEntry = [line[2]] # append common name regardless of visualization type
-                dataToSend.append(line[2])
+                dataEntry = [line[2]] 
                 if visType == "bodymass":
-                    dataToSend.append(float(line[5])) #append body mass
+                    dataEntry.append(float(line[5])) #append body mass
                 elif visType == "diet":
-                    dataToSend.append(line[4])
+                    dataEntry.append(line[4])
                 else:
                     print("error")
                 # dataToSend.append(dataEntry)
+                dataToSend[int(lineNum)] = dataEntry
             lineNum += 1
+    with open("wetland_diet.json", "w") as fileName:
+        json.dump(dataToSend, fileName)
     return dataToSend
 
 
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='ip-address', port=8000) # change the host name
-    # print(dataUploader())
+    print(dataUploader("diet", "wetland.csv"))
